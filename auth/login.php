caa03246b2f77d540e464f83dec6ab03b82ccf4f
Marocato <60039981+Noureddine17@users.noreply.github.com>
2026-05-11 01:27:09 +0200

    Add logging requirement to login.php

--- auth/login.php
+++ auth/login.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 modiflog(true,$_POST['email']);
-
+require_once(__DIR__ . '/../config/logs.php');
 require_once(__DIR__ . '/../config/connexion.php');
 require_once(__DIR__ . '/../config/functions.php');
 
