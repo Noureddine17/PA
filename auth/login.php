e362c1cffc1ab5f38862355da6c44a45265cee4f
Marocato <60039981+Noureddine17@users.noreply.github.com>
2026-05-11 01:38:46 +0200

    Remove email logging from login process
    Removed logging function call for email on login.

--- auth/login.php
+++ auth/login.php
@@ -1,9 +1,7 @@
 <?php
 session_start();
-require_once(__DIR__ . '/../config/logs.php');
 require_once(__DIR__ . '/../config/connexion.php');
 require_once(__DIR__ . '/../config/functions.php');
-modiflog(true,$_POST['email']);
 
 $errors = [];
 
