449be0f696389690047637670501cdb760c34a7c
Marocato <60039981+Noureddine17@users.noreply.github.com>
2026-06-16 14:42:54 +0200

    Remove modiflog function from logs.php

--- config/logs.php
+++ config/logs.php
@@ -1,9 +1,4 @@
 <?php
-function modiflog($success,$mail){
-    $stream=fopen("log.txt","a");
-    $line=date("Y-m-d - H:i:s")."-Tentative de connexion " .($success ? 'réussie de : ' : 'échouée de : ') .$mail."\n";
-    fputs($stream,$line);
-    fclose($stream);
-}
+//
 
 ?>
