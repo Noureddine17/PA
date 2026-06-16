46e17c95f235e9fa354f35c1c1c2fe3493f24b42
Marocato <60039981+Noureddine17@users.noreply.github.com>
2026-06-16 14:31:39 +0200

    chaning logs place

--- config/functions.php
+++ config/functions.php
@@ -30,3 +30,12 @@ function sendMail($to, $subject, $message)
 
     return mail($to, $subject, $message, $headers);
 }
+
+
+function modiflog($success,$mail){
+    $stream=fopen("log.txt","a");
+    $line=date("Y-m-d - H:i:s")."-Tentative de connexion " .($success ? 'réussie de : ' : 'échouée de : ') .$mail."\n";
+    fputs($stream,$line);
+    fclose($stream);
+}
+
