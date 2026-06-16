5c6a83d4ac0b16a9a32d66711b0281e8d499705e
Marocato <60039981+Noureddine17@users.noreply.github.com>
2026-06-16 14:50:03 +0200

    Update log file path in modiflog function

--- config/functions.php
+++ config/functions.php
@@ -33,7 +33,7 @@ function sendMail($to, $subject, $message)
 
 
 function modiflog($success,$mail){
-    $stream=fopen("log.txt","a");
+    $stream=fopen("../log.txt","a");
     $line=date("Y-m-d - H:i:s")."-Tentative de connexion " .($success ? 'réussie de : ' : 'échouée de : ') .$mail."\n";
     fputs($stream,$line);
     fclose($stream);
