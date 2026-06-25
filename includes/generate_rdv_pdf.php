248c64cadcf4c02bd12cdd5cec4aa3598e4b169d
Noureddine <nehammouch@iliad-free.fr>
2026-06-25 17:39:24 +0200

    fix utf8_decode obselete

--- includes/generate_rdv_pdf.php
+++ includes/generate_rdv_pdf.php
@@ -9,7 +9,6 @@ if (!isset($_GET['id_rdv']) || !is_numeric($_GET['id_rdv'])) {
 }
 
 $idRdv = (int)$_GET['id_rdv'];
-
 $stmt = $pdo->prepare('
     SELECT
         R.id_rdv,
@@ -29,6 +28,7 @@ $stmt = $pdo->prepare('
     JOIN UTILISATEUR E ON R.id_expert = E.id_user
     WHERE R.id_rdv = ?
 ');
+
 $stmt->execute([$idRdv]);
 $rdv = $stmt->fetch(PDO::FETCH_ASSOC);
 
@@ -38,30 +38,28 @@ if (!$rdv) {
 }
 
 $splithour = explode(':', $rdv['heure']);
-$heure = $splithour[0]. ':' . $splithour[1];
+$heure = $splithour[0] . ':' . $splithour[1];
 
 $pdf = new FPDF();
 $pdf->AddPage();
 $pdf->SetFont('Arial', 'B', 16);
-$pdf->Cell(0, 10 , utf8_decode('Récapitulatif de votre rendez-vous KAESKIN'), 1, 1, 'C');
+$pdf->Cell(0, 10, mb_convert_encoding('Récapitulatif de votre rendez-vous KAESKIN', 'ISO-8859-1', 'UTF-8'), 1, 1, 'C');
 $pdf->Ln(10);
-
 $pdf->SetFont('Arial', '', 12);
-$pdf->Cell(0, 10, utf8_decode('Informations du client :'), 0, 1);
-$pdf->Cell(0, 7, utf8_decode('Nom : ' . $rdv['client_prenom'] . ' ' . $rdv['client_nom']), 0, 1);
-$pdf->Cell(0, 7, utf8_decode('Email : ' . $rdv['client_email']), 0, 1);
+$pdf->Cell(0, 10, mb_convert_encoding('Informations du client :', 'ISO-8859-1', 'UTF-8'), 0, 1);
+$pdf->Cell(0, 7, mb_convert_encoding('Nom : ' . $rdv['client_prenom'] . ' ' . $rdv['client_nom'], 'ISO-8859-1', 'UTF-8'), 0, 1);
+$pdf->Cell(0, 7, mb_convert_encoding('Email : ' . $rdv['client_email'], 'ISO-8859-1', 'UTF-8'), 0, 1);
 $pdf->Ln(5);
-
-$pdf->Cell(0, 10, utf8_decode('Informations du rendez-vous :'), 0, 1);
-$pdf->Cell(0, 7, utf8_decode('Service : ' . $rdv['service']), 0, 1);
-$pdf->Cell(0, 7, utf8_decode('Expert : ' . $rdv['expert_prenom'] . ' ' . $rdv['expert_nom']), 0, 1);
-$pdf->Cell(0, 7, utf8_decode('Date : ' . $rdv['date_rdv']), 0, 1);
-$pdf->Cell(0, 7, utf8_decode('Heure : ' . $heure), 0, 1);
-$pdf->Cell(0, 7, utf8_decode('Durée : ' . $rdv['duree']), 0, 1);
-$pdf->Cell(0, 7, utf8_decode('Prix : ' . number_format($rdv['prix'], 2, ',', '.') . ' €'), 0, 1);
-$pdf->Cell(0, 7, utf8_decode('Mode de paiement : ' . $rdv['mode_paiement']), 0, 1);
+$pdf->Cell(0, 10, mb_convert_encoding('Informations du rendez-vous :', 'ISO-8859-1', 'UTF-8'), 0, 1);
+$pdf->Cell(0, 7, mb_convert_encoding('Service : ' . $rdv['service'], 'ISO-8859-1', 'UTF-8'), 0, 1);
+$pdf->Cell(0, 7, mb_convert_encoding('Expert : ' . $rdv['expert_prenom'] . ' ' . $rdv['expert_nom'], 'ISO-8859-1', 'UTF-8'), 0, 1);
+$pdf->Cell(0, 7, mb_convert_encoding('Date : ' . $rdv['date_rdv'], 'ISO-8859-1', 'UTF-8'), 0, 1);
+$pdf->Cell(0, 7, mb_convert_encoding('Heure : ' . $heure, 'ISO-8859-1', 'UTF-8'), 0, 1);
+$pdf->Cell(0, 7, mb_convert_encoding('Durée : ' . $rdv['duree'], 'ISO-8859-1', 'UTF-8'), 0, 1);
+$pdf->Cell(0, 7, mb_convert_encoding('Prix : ' . number_format($rdv['prix'], 2, ',', '.') . ' €', 'ISO-8859-1', 'UTF-8'), 0, 1);
+$pdf->Cell(0, 7, mb_convert_encoding('Mode de paiement : ' . $rdv['mode_paiement'], 'ISO-8859-1', 'UTF-8'), 0, 1);
 $pdf->Ln(10);
 
 $pdf->Output('I', 'rdv_kaeskin_' . $idRdv . '.pdf');
+
 exit;
-?>
